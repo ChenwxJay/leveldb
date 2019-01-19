238102944a57787b14d55e5b50d57d6d990c7934
ChenwxJay <chenwx_jay@163.com>
2019-01-19 23:58:30 +0800

    阅读arena内存池实现源码，增加部分注释

--- util/arena.cc
+++ util/arena.cc
@@ -2,67 +2,80 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#include "util/arena.h"
-#include <assert.h>
+#include "util/arena.h" //头文件声明
+#include <assert.h> //加断言
 
 namespace leveldb {
 
-static const int kBlockSize = 4096;
+static const int kBlockSize = 4096;//定义块大小，使用静态const修饰
 
 Arena::Arena() : memory_usage_(0) {
+  //构造函数只需要初始化内部成员
   alloc_ptr_ = nullptr;  // First allocation will allocate a block
   alloc_bytes_remaining_ = 0;
 }
 
 Arena::~Arena() {
+  //析构函数，释放所有内存，利用delete
   for (size_t i = 0; i < blocks_.size(); i++) {
+	//遍历存储block的数组
     delete[] blocks_[i];
   }
 }
 
-char* Arena::AllocateFallback(size_t bytes) {
+//回调函数之一
+char* Arena::AllocateFallback(size_t bytes) { 
+  //需要分配的块大小超过固定的四分之一，就在新的内存块中分配
   if (bytes > kBlockSize / 4) {
     // Object is more than a quarter of our block size.  Allocate it separately
     // to avoid wasting too much space in leftover bytes.
-    char* result = AllocateNewBlock(bytes);
-    return result;
+    char* result = AllocateNewBlock(bytes);//直接
+    return result;//返回分配的指针
   }
 
+  //剩下的空间丢弃，重新分配一个固定内存块，在新的内存块中分配
   // We waste the remaining space in the current block.
-  alloc_ptr_ = AllocateNewBlock(kBlockSize);
-  alloc_bytes_remaining_ = kBlockSize;
+  alloc_ptr_ = AllocateNewBlock(kBlockSize);//创建一个新的块，由alloc_ptr_接管
+  alloc_bytes_remaining_ = kBlockSize;//设置剩余空间大小
 
-  char* result = alloc_ptr_;
-  alloc_ptr_ += bytes;
+  char* result = alloc_ptr_;//赋值
+  alloc_ptr_ += bytes;//指针偏移bytes个字节
   alloc_bytes_remaining_ -= bytes;
   return result;
 }
 
-char* Arena::AllocateAligned(size_t bytes) {
+char* Arena::AllocateAligned(size_t bytes) {//分配对齐空间
+  //根据void*的大小来决定使用对齐字节数
   const int align = (sizeof(void*) > 8) ? sizeof(void*) : 8;
+  //必须保证是2的幂次
   assert((align & (align-1)) == 0);   // Pointer size should be a power of 2
+  //计算当前地址的低位 
   size_t current_mod = reinterpret_cast<uintptr_t>(alloc_ptr_) & (align-1);
-  size_t slop = (current_mod == 0 ? 0 : align - current_mod);
-  size_t needed = bytes + slop;
+  
+  size_t slop = (current_mod == 0 ? 0 : align - current_mod);//计算差值
+  size_t needed = bytes + slop;//需要多分配的字节数
   char* result;
-  if (needed <= alloc_bytes_remaining_) {
-    result = alloc_ptr_ + slop;
-    alloc_ptr_ += needed;
-    alloc_bytes_remaining_ -= needed;
+  if (needed <= alloc_bytes_remaining_) {//需要分配的字节数小于当前块剩余空间
+    result = alloc_ptr_ + slop;//地址偏移
+    alloc_ptr_ += needed;//地址偏移所需要的字节
+    alloc_bytes_remaining_ -= needed;//剩余空间减少
   } else {
     // AllocateFallback always returned aligned memory
-    result = AllocateFallback(bytes);
+    result = AllocateFallback(bytes);//调用回调函数，默认返回对齐的内存
   }
-  assert((reinterpret_cast<uintptr_t>(result) & (align-1)) == 0);
-  return result;
+  assert((reinterpret_cast<uintptr_t>(result) & (align-1)) == 0);//已经对齐
+  return result;//返回对齐的内存块首地址
 }
 
-char* Arena::AllocateNewBlock(size_t block_bytes) {
+char* Arena::AllocateNewBlock(size_t block_bytes) {//参数是需要分配的大小
+  //使用new动态分配内存，大小为参数指定
   char* result = new char[block_bytes];
+  //加入vector
   blocks_.push_back(result);
-  memory_usage_.NoBarrier_Store(
+  memory_usage_.NoBarrier_Store( //更改原子类型变量
+       //修改原有的原子类型值，再存储
       reinterpret_cast<void*>(MemoryUsage() + block_bytes + sizeof(char*)));
-  return result;
+  return result;//返回分配内存的首地址
 }
 
 }  // namespace leveldb
--- util/arena.h
+++ util/arena.h
@@ -5,10 +5,10 @@
 #ifndef STORAGE_LEVELDB_UTIL_ARENA_H_
 #define STORAGE_LEVELDB_UTIL_ARENA_H_
 
-#include <vector>
-#include <assert.h>
-#include <stddef.h>
-#include <stdint.h>
+#include <vector> //vector头文件
+#include <assert.h> 
+#include <stddef.h> //定义
+#include <stdint.h> //标准头文件
 #include "port/port.h"
 
 namespace leveldb {
@@ -19,34 +19,35 @@ class Arena {
   ~Arena();
 
   // Return a pointer to a newly allocated memory block of "bytes" bytes.
-  char* Allocate(size_t bytes);
+  char* Allocate(size_t bytes); //分配内存，以字节为单位
 
   // Allocate memory with the normal alignment guarantees provided by malloc
-  char* AllocateAligned(size_t bytes);
+  char* AllocateAligned(size_t bytes);//分配对齐的内存，底层采用malloc实现
 
   // Returns an estimate of the total memory usage of data allocated
   // by the arena.
   size_t MemoryUsage() const {
+	//返回当前内存使用情况，需要使用原子加载
     return reinterpret_cast<uintptr_t>(memory_usage_.NoBarrier_Load());
   }
 
  private:
-  char* AllocateFallback(size_t bytes);
-  char* AllocateNewBlock(size_t block_bytes);
+  char* AllocateFallback(size_t bytes);//分配回调函数
+  char* AllocateNewBlock(size_t block_bytes);//新的块分配回调函数
 
   // Allocation state
-  char* alloc_ptr_;
-  size_t alloc_bytes_remaining_;
+  char* alloc_ptr_;//分配指针
+  size_t alloc_bytes_remaining_;//指定剩余多少字节可分配空间
 
   // Array of new[] allocated memory blocks
-  std::vector<char*> blocks_;
+  std::vector<char*> blocks_;//底层是一个vector
 
   // Total memory usage of the arena.
-  port::AtomicPointer memory_usage_;
+  port::AtomicPointer memory_usage_;//使用原子类型，记录内存使用情况
 
   // No copying allowed
-  Arena(const Arena&);
-  void operator=(const Arena&);
+  Arena(const Arena&);//拷贝构造函数
+  void operator=(const Arena&);//赋值运算符，私有
 };
 
 inline char* Arena::Allocate(size_t bytes) {
@@ -54,13 +55,15 @@ inline char* Arena::Allocate(size_t bytes) {
   // 0-byte allocations, so we disallow them here (we don't need
   // them for our internal use).
   assert(bytes > 0);
+  
   if (bytes <= alloc_bytes_remaining_) {
-    char* result = alloc_ptr_;
+    char* result = alloc_ptr_; //保存当前分配的内存地址
     alloc_ptr_ += bytes;
-    alloc_bytes_remaining_ -= bytes;
-    return result;
+    alloc_bytes_remaining_ -= bytes;//可分配字节数减少
+    return result;//返回分配的起始指针
   }
-  return AllocateFallback(bytes);
+  //如果所需内存太大，则调用另外的函数进行分配
+  return AllocateFallback(bytes);//需要调用回调函数
 }
 
 }  // namespace leveldb
